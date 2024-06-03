2cb283a59fb428eaf024a610160bcc9b0a15e3ef
Zac Durham <43076672+PapaZ810@users.noreply.github.com>
2024-06-03 13:03:56 -0600

    user verification

--- timelapse/views.py
+++ timelapse/views.py
@@ -7,11 +7,13 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class IndexView(TemplateView):
+class IndexView(LoginRequiredMixin, TemplateView):
+    login_url = '/'
     template_name = 'timelapse/index.html'
 
 
-class UploadView(FormView):
+class UploadView(LoginRequiredMixin, FormView):
+    login_url = '/'
     template_name = 'timelapse/upload.html'
     form_class = UploadForm
     success_url = '/index/'
@@ -23,20 +25,21 @@ class UploadView(FormView):
 class LoginView(FormView):
     template_name = 'timelapse/login.html'
     form_class = LoginForm
-    success_url = '/index/'
+    success_url = '/home/'
 
     def form_valid(self, form):
         username = form.cleaned_data['username']
         password = form.cleaned_data['password']
+        print('god help me')
 
         user = authenticate(self.request, username=username, password=password)
 
         if user is not None:
-            login(self.request, user, backend='django.contrib.auth.backends.ModelBackend')
+            login(self.request, user)
             return super().form_valid(form)
         else:
             print("Invalid username or password")
-            form.add_error(None, "Invalid username or password")
+            form.add_error('password', "Invalid username or password")
             return self.form_invalid(form)
 
 
